3912623406503f179703482a14528008141bc00a
TongTianDaXia <linyinglll@qq.com>
2018-05-20 13:57:34 +0800

    add interface of fgmres of mkl

--- src/BasicMathLib/laWrapperLib.f90
+++ src/BasicMathLib/laWrapperLib.f90
@@ -1,8 +1,13 @@
+!dir$ if .not. defined(noMKL)
+include 'mkl_rci.f90'
+!dir$ end if
+    
 module laWrapperLib
 use constants
 use arrayOpsLib
-!dir$ if .not. defined(without_imkl)
+!dir$ if .not. defined(noMKL)
 use lapack95
+use mkl_rci
 !dir$ end if
 implicit none
 
@@ -12,9 +17,13 @@ implicit none
     public:: solveGeneralLES
     public:: solveTridiagonalLES
     public:: triFactorSquareMat
+    !--
+    public:: csrAx
+    public:: fgmres
+    public:: fgmresILUT
     
     !totally rely on the lapack
-!dir$ if .not. defined(without_imkl)
+!dir$ if .not. defined(noMKL)
     public:: solveSymmetryLES
     public:: eigenSymTriDiagonal
     public:: inverseGeneralSquareMat
@@ -57,20 +66,18 @@ contains
     ![evc]  output n*n orthonomal eigenvector
     !Find all eigenvalues and eigenvectors of a tridiagonal matrix T
     pure subroutine eigenTriDiagonal(d,e,evc)
-    real(rp),dimension(:),intent(inout)::                           d
-    real(rp),dimension(:),intent(in)::                              e
-    complex(rp),dimension(:,:),intent(out),optional::               evc
-    
+    real(rp),dimension(:),intent(inout)::               d
+    real(rp),dimension(:),intent(in)::                  e
+    complex(rp),dimension(:,:),intent(out),optional::   evc
         if(present(evc)) then
             call steqr(d,e,evc)
         else
             call steqr(d,e)
         endif
-    
     end subroutine eigenTriDiagonal
     
     
-!dir$ if defined (without_imkl)
+!dir$ if defined (noMKL)
     !because triFacotrSquareMat may lead U be singluar, this procedure may fails
     pure subroutine solveGeneralLES(a,b)
     real(rp),dimension(:,:),intent(inout)::     a
@@ -91,34 +98,33 @@ contains
         enddo
     end subroutine solveGeneralLES
 !dir$ else
+    !refer to ?getrf of lapack
     !getrf is short for triangular factorization of general real matrices
     !solve the general linear equations system by triangular factorization
     pure subroutine solveGeneralLES(a,b)
     real(rp),dimension(1:,1:),intent(inout)::   a
     real(rp),dimension(1:),intent(inout)::      b
     integer(isp),allocatable,dimension(:)::     ipiv
-        !refer to ?getrf of lapack
-        allocate( ipiv( max(1,min(size(a,dim=1),size(a,dim=2))) ) )
+        allocate(ipiv(max(1 , min(size(a,dim=1) , size(a,dim=2)))))
         call getrf(a,ipiv)
         call getrs(a,ipiv,b)
     end subroutine solveGeneralLES
 !dir$ end if
     
     
-    
-!dir$ if defined (without_imkl)
+!dir$ if defined (noMKL)
 !dir$ else
     pure subroutine inverseGeneralSquareMat(a)
     real(rp),dimension(:,:),intent(inout)::     a
     integer(isp),allocatable,dimension(:)::     ipiv
-        allocate(ipiv(max(1,min(size(a,dim=1),size(a,dim=2)))))
+        allocate(ipiv(max(1 , min(size(a,dim=1) , size(a,dim=2)))))
         call getrf(a,ipiv)
         call getri(a,ipiv)
     end subroutine inverseGeneralSquareMat
 !dir$ end if
     
     
-!dir$ if defined (without_imkl)
+!dir$ if defined (noMKL)
     !input a [(2:n,1),(1:n,2),(1:n-1,3)]
     !refer to chasing method
     !limiting: abs(a)=>(a(1,2)>a(1,3)>0),((a(i,2)>a(i,1)+a(i,3)),(a(n,2)>a(n,1))
@@ -206,14 +212,15 @@ contains
     !y_m = argmin_y | \beta e_1 - \hat{H}_m y |_2
 !-------------------------------------------------
     !solve AX=b, A is sparse matrix, full size (n*n)
-    !nnz means number of non-zero
+    ![nnz] means number of non-zero
     !csr structure means compressed sparse row, consists of three parts(a(nnz),ia(n+1),ja(nnz))
     !a: the value of non-zero
-    !ia: the start of a row of the sparse matrix in <nnz> dimension
+    !ia: the start of a row of the sparse matrix in <nnz> dimension, range(1:nnz)
     !ja: the location in this row, range (1:n)
     !maxInr range (1:n)
-    pure subroutine gmres(a,ia,ja,b,maxOtr,maxInr, x)
-    real(rp),dimension(:),intent(in)::      a,ia,ja,b
+    subroutine gmres(a,ia,ja,b,maxOtr,maxInr, x)
+    real(rp),dimension(:),intent(in)::      a,b
+    integer(ip),dimension(:),intent(in)::   ia,ja
     integer(ip),intent(in)::                maxOtr,maxInr
     real(rp),dimension(:),intent(inout)::   x
     real(rp),parameter::                    rEps = 10._rp * GlobalEps
@@ -227,8 +234,8 @@ contains
     
         !outer loop
         do iter = 1,maxOtr
-        
-            r = b - Ax(a,ia,ja,x)
+            
+            call csrAx(a,ia,ja,x,r); r = b - r
             beta = norm2(r)
             if(iter==1) eps = beta * rEps
             v(:,1) = r / beta
@@ -241,7 +248,7 @@ contains
             
                 jcopy = j
             
-                v(:,j+1) = Ax(a,ia,ja,v(:,j))
+                call csrAx(a,ia,ja,v(:,j),v(:,j+1))
                 av = norm2(v(:,j+1))
                 
                 !--orth
@@ -300,20 +307,6 @@ contains
         
     contains
     
-        !compute Ax with csr form of A
-        pure function Ax(a,ia,ja,x)
-        real(rp),dimension(:),intent(in)::  a,ia,ja,x
-        real(rp),dimension(size(x))::       Ax
-        integer(ip)::                       i,j,k
-            Ax = 0._rp
-            do i=1,size(r)
-                j = ia(i);
-                k = ia(i+1) - 1
-                Ax(i) = Ax(i) + (a(j:k) .ip. x(ja(j:k)))
-            enddo
-        end function Ax
-
-        
         !--
         pure subroutine Arnoldi(v,H,ksp)
         real(rp),dimension(:),intent(in)::      v
@@ -328,11 +321,178 @@ contains
         pure subroutine HouseholderArnoldi()
         !real(rp),dimension(:),intent(in)::      
         
-        
-        
-        
         end subroutine HouseholderArnoldi
         
     end subroutine gmres
 
+!-----------------------------------------------    
+!dir$ if .not. defined(noMKL)
+    !Ax=b | A=A(a,ia,ja) | input x0 output x |
+    subroutine fgmresILUT(a,ia,ja,b,x)
+    real(8),dimension(:),intent(in)::       a
+    integer(4),dimension(:),intent(in)::    ia,ja
+    real(8),dimension(:),intent(inout)::    x,b
+    real(8),dimension(size(x))::            trv
+    integer(4)::                            n,nm1,rciRequest,iter,ip15,maxfil,ierr,itercount
+    integer(4),dimension(128)::             ipar
+    integer(4),dimension(:),allocatable::   ibilut,jbilut
+    real(8)::                               tol
+    real(8),dimension(128)::                dpar
+    real(8),dimension(:),allocatable::      tmp,bilut
+    
+        n = size(x); nm1 = n-1
+        !--
+        maxfil = 1  !half bandwidth of preconditioner M=LU
+        allocate(bilut((2*maxfil+1)*n - maxfil*(maxfil+1) + 1))
+        allocate(jbilut((2*maxfil+1)*n - maxfil*(maxfil+1) + 1))
+        allocate(ibilut(n+1))
+        
+        !--
+        ip15 = min(2,n) !restarted gmres, default min(150,n) which means no restarted gmres
+                        !sometimes small ip15 is sufficient for convergence and sometimes large ipar15 is necessary
+        allocate(tmp((2*ip15+1)*n + pidb2(ip15*(ip15+9)) + 1))
+        
+        !--
+        call dfgmres_init(n, x, b, rciRequest, ipar, dpar, tmp)
+        
+        call preconditioner
+        
+        call setCheck
+        
+        !loop
+        do; call dfgmres(n,x,b,rciRequest,ipar,dpar,tmp)
+            select case(rciRequest)
+            case(0)
+                exit
+            case(1)
+                call csrAx(a,ia,ja , tmp(ipar(22):ipar(22)+nm1) , tmp(ipar(23):ipar(23)+nm1))
+            case(2) !stopping test
+                stop 'error: laWrapperLib/fgmresILUT has no user-defined stopping test, ipar(10)=0'
+            case(3)
+                call mkl_dcsrtrsv('l','n','u',n,bilut,ibilut,jbilut,tmp(ipar(22)),trv)
+                call mkl_dcsrtrsv('u','n','n',n,bilut,ibilut,jbilut,trv,tmp(ipar(23)))
+            case(4)
+                if(dpar(7)<1.e-12_8) exit   !norm of generated vector
+            case default
+                print*, 'fgmres request:  ',rciRequest
+                stop 'error: laWrapperLib/fgmresILUT fails'
+            end select
+        enddo
+        
+        ipar(13) = 0 !update x
+        call dfgmres_get(n, x, b, rciRequest, ipar, dpar, tmp, itercount)
+        call mkl_free_buffers
+        
+    contains
+        !--
+        subroutine preconditioner
+            ipar(31) = 1        !0:abort ilu of routine meets zero diag element; 1: plus dpar(31)*norm(ev)
+            dpar(31) = 1.e-7_8  !diag value
+            tol = 1.e-8_8       !tolerance
+            call dcsrilut(n, a, ia, ja, bilut, ibilut, jbilut, tol, maxfil, ipar, dpar, ierr)
+        end subroutine preconditioner
+        !--
+        subroutine SetCheck
+            ipar(8) = 0         !not perform stop testing for loop | ipar(4)[current outer loop] < ipar(5)[min(150,n)]
+            ipar(9) = 1         !not perform stop testing for accuracy | dpar(5)[currentResNorm] < dpar(4)
+                                != dpar(1)*dpar(3)_0 + dpar(2) [relativeTol * initResNorm + absoluteTol]
+            ipar(10) = 0        !do perform user-defined stopping test
+            ipar(11) = 1        !0: run non-preconditioned dfgmres; 1: run preconditioned dfgmres
+            ipar(12) = 0        !not perform norm of currently generated vecotr | dpar(7) < dpar(8) [1.e-12_8]
+            ipar(15) = ip15     !restarted gmres[inner loop], default min(150,n) which means no restarted gmres
+            dpar(1) = 1.e-8_8
+            call dfgmres_check(n, x, b, rciRequest, ipar, dpar, tmp)
+        end subroutine setCheck
+    end subroutine fgmresILUT
+    
+    subroutine fgmres(a,ia,ja,b,x)
+    real(8),dimension(:),intent(in)::       a
+    integer(4),dimension(:),intent(in)::    ia,ja
+    real(8),dimension(:),intent(inout)::    x,b
+    integer(4)::                            n,nm1,rciRequest,ip15,itercount
+    integer(4),dimension(128)::             ipar
+    real(8),dimension(128)::                dpar
+    real(8),dimension(:),allocatable::      tmp
+    
+        n = size(x); nm1 = n-1
+        !--
+        ip15 = min(2,n) !restarted gmres, default min(150,n) which means no restarted gmres
+                        !sometimes small ip15 is sufficient for convergence and sometimes large ipar15 is necessary
+        allocate(tmp((2*ip15+1)*n + pidb2(ip15*(ip15+9)) + 1))
+
+        !--
+        call dfgmres_init(n, x, b, rciRequest, ipar, dpar, tmp)
+        
+        call setCheck
+        
+        !loop
+        do; call dfgmres(n,x,b,rciRequest,ipar,dpar,tmp)
+            select case(rciRequest)
+            case(-1)
+                print*, 'warning: laWrapperLib/fgmres get a bad initial guess, solution may be inaccurate'
+                exit !
+            case(0)
+                exit
+            case(1)
+                call csrAx(a,ia,ja , tmp(ipar(22):ipar(22)+nm1) , tmp(ipar(23):ipar(23)+nm1))
+            case(2) !stopping test
+                stop 'error: laWrapperLib/fgmres has no user-defined stopping test, ipar(10)=0'
+            case(3)
+                stop 'error: laWrapperLib/fgmres has no user-defined preconditioner'
+            case(4)
+                stop 'error: laWrapperLib/fgmres has no user-defined vector check'
+            case default
+                print*, 'fgmres request:  ',rciRequest
+                stop 'error: laWrapperLib/fgmres fails'
+            end select
+        enddo
+        
+        ipar(13) = 0 !update x
+        call dfgmres_get(n, x, b, rciRequest, ipar, dpar, tmp, itercount)
+        call mkl_free_buffers
+        
+    contains
+        !--
+        subroutine SetCheck
+            ipar(8) = 1         !perform stop testing for loop | ipar(4)[current outer loop] < ipar(5)[min(150,n)]
+            ipar(9) = 1         !perform stop testing for accuracy | dpar(5)[currentResNorm] < dpar(4)
+                                != dpar(1)*dpar(3) + dpar(2) [relativeTol * initResNorm + absoluteTol]
+            ipar(10) = 0        !do not perform user-defined stopping test
+            ipar(11) = 0        !0: run non-preconditioned dfgmres; 1: run preconditioned dfgmres
+            ipar(12) = 1        !perform norm of currently generated vecotr | dpar(7) < dpar(8) [1.e-12_8]
+            ipar(15) = ip15     !restarted gmres[inner loop], default min(150,n) which means no restarted gmres
+            dpar(1) = 1.e-8_8   !relative tolerance
+            dpar(2) = 0._8      !abolute tolerance
+            call dfgmres_check(n, x, b, rciRequest, ipar, dpar, tmp)
+        end subroutine setCheck
+    end subroutine fgmres
+!dir$ else
+    
+!dir$ end if
+    
+    
+!dir$ if .not. defined(noMKL)
+    !ge[sparse representation of a general matrix]
+    !mv[matrix-vector product ]
+    subroutine csrAx(a,ia,ja,x,b)
+    real(rp),dimension(:),intent(in)::      a,x
+    integer(ip),dimension(:),intent(in)::   ia,ja
+    real(rp),dimension(:),intent(out)::     b
+        call mkl_dcsrgemv('n', size(x), a, ia, ja, x, b)
+    end subroutine csrAx
+!dir$ else
+    pure subroutine csrAx(a,ia,ja,x,b)
+    real(rp),dimension(:),intent(in)::      a,x
+    integer(ip),dimension(:),intent(in)::   ia,ja
+    real(rp),dimension(:),intent(out)::     b
+    integer(ip)::                           i,j,k
+        b = 0._rp
+        do i=1,size(x)
+            j = ia(i);
+            k = ia(i+1) - 1
+            b(i) = b(i) + (a(j:k) .ip. x(ja(j:k)))
+        enddo
+    end subroutine csrAx
+!dir$ end if
+    
 end module laWrapperLib
